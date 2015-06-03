04bd25304ab7f88b9809aa47b20db2eb4876bedf
Joseph Irwin <joseph.irwin.gt@gmail.com>
2015-06-03 16:22:05 +0900

    Apply trx to PTB

--- main.rkt
+++ main.rkt
@@ -1,11 +1,22 @@
 #lang racket/base
 
 (require hage/parser)
-(provide (all-from-out hage/parser))
+(require hage/trx)
 
-(define (tree-traverse tree visit initial-state)
-  (if (leaf? tree)
-      (visit tree initial-state empty)
-      (visit tree
-             initial-state
-             (???))))
+
+(define (np? x) (equal? "NP" (substring x 0 2)))
+(define (! pred?) (lambda (x) (not (pred? x))))
+
+(define pat (trx (^ ,np? (+ (any)))))
+(define non-recursive (trx (^ ,np? (+ (rec notnp (^ ,(! np?) (+ notnp)))))))
+
+(define tree (car (ptb-read (open-input-file "/home/joseph/Data/PTB/combined/wsj/00/wsj_0003.mrg"))))
+
+(define npsbj (cadr 
+               (cadr
+                (caadr tree))))
+(define basenp (cadr (cadr npsbj)))
+
+(trx-match pat npsbj)
+(trx-match non-recursive npsbj)
+(trx-match non-recursive basenp)
