97070da48a1218af46b436ca85e352d50e084e96
Joseph Irwin <joseph.irwin.gt@gmail.com>
2015-06-10 12:18:21 +0900

    Hack on

--- main.rkt
+++ main.rkt
@@ -56,6 +56,9 @@
        (constituent (parse-label label) '() (map helper children))]))
   (tree (helper sxp)))
 
+
+(define (annotate-tree t proc init) #f)
+
 ;; ---------------------------------------------------------------------------------------------------
 ;; experiments
 
